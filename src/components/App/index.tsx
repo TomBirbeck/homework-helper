884a6aeb75cf3acee7432fcd0cf4a6c6416e7f30
unknown <tombirbeck47@gmail.com>
2022-11-24 08:39:10 +0000

    installed tailwindcss

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,16 +4,19 @@ import './App.css'
 import StudentHomepage from '../StudentHomepage'
 import ParentHomepage from '../ParentHomepage'
 import NewTaskForm from '../NewTaskForm'
+import Tasks from '../Tasks'
+import TaskList from '../TasksList'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="m-10">
       <LoginPage/>
       <StudentHomepage/>
       <ParentHomepage/>
       <NewTaskForm/>
-        
+      <Tasks/>
+      <TaskList/>
     </div>
   )
 }
---
+++ src/components/TasksList/index.tsx
@@ -0,0 +1,7 @@
+const TaskList = () => {
+    return (
+        <h1>mapped item list here</h1>
+    )
+}
+
+export default TaskList
