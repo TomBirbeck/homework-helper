ceac1949625acfdeda27adce492b91f6fbabe038
unknown <tombirbeck47@gmail.com>
2022-12-06 00:02:29 +0000

    api call works on the frontend

--- src/components/ParentHomepage/index.tsx
+++ src/components/ParentHomepage/index.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react"
+
 const ParentHomepage = () => {
+
+    const [api, setApi] = useState<Array<[]>>()
+
+    useEffect( () => {
+        async function dbTest(){
+            const res = await fetch('https://homeworkhelper.onrender.com/student')
+                const data = await res.json()
+                setApi(data.payload)
+        } 
+        dbTest()
+        },[])
+
+        console.log(api)
     return (
         <h1>Hello from Parent Homepage</h1>
     )
