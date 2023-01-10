3a82ce71b46f01ac93939ec2dd6d5aad6023374e
unknown <tombirbeck47@gmail.com>
2023-01-10 18:33:36 +0000

    styled new task form for mobile and pc

--- src/components/NewTaskForm/index.tsx
+++ src/components/NewTaskForm/index.tsx
@@ -63,39 +63,39 @@ const NewTaskForm = ({ createTask}: formProps) => {
           });
         }}
         // className='w-1/2 h-60 grid grid-flow-row-dense grid-cols-3 border-solid rounded-lg border-2 border-black bg-yellow-300 space-around p-2 my-2'
-        className='z-20 w-100 mb:w-1/2 h-60 grid grid-cols-2 md:grid-cols-3 bg-none backdrop-blur-sm border-solid border-2 border-opacity-10 border-white rounded-lg text-white space-around p-2 mb:p-6 my-2'
+        className='z-20 w-full md:w-fit mb:w-1/2 h-fit md:h-60 grid grid-cols-1 md:grid-cols-3 bg-none backdrop-blur-sm border-solid border-2 border-opacity-10 border-white rounded-lg text-white space-around p-2 mb:p-6 my-2'
       >
-        <label htmlFor='subject' className='flex flex-wrap gap-1 col-span-2 md:col-span-1'>
+        <label htmlFor='subject' className='col-span-3 md:col-span-1 place-self-start'>
           Subject:
           <input
             required
             type='text'
             value={newTask.subject}
-            className='border-solid border-2 border-white w-24 h-7 bg-transparent pl-1 pb-1 text-white'
+            className='border-solid border-2 border-white col-span-1 md:w-32 md:mr-4 ml-2 h-7 bg-transparent pl-1 pb-1 text-white'
             placeholder='subject'
             onChange={handleSubject}
           ></input>
         </label>
-        <label htmlFor='topic' className='flex flex-wrap gap-1 place-self-center'>
-          Topic:
+        <label htmlFor='topic' className='col-span-3 md:col-span-1 place-self-start'>
+          Topic: 
           <input
             type='text'
             value={newTask.topic}
-            className='border-solid border-2 border-white w-24 h-7 bg-transparent pl-1 pb-1 text-white'
+            className='border-solid border-2 border-white col-span-1 md:w-32 ml-6 md:ml-2 mt-1 md:mt-0 h-7 bg-transparent pl-1 pb-1 text-white'
             placeholder='topic'
             onChange={handleTopic}
           ></input>
         </label>
-        <label htmlFor='date' className='col-span-2 md:col-span-1 place-self-start'>
+        <label htmlFor='date' className='col-span-3 md:col-span-1 place-self-start'>
           Due:
           <input
             type='date'
             value={newTask.due}
-            className='border-solid border-2 border-white w-32 h-7 bg-transparent pl-1 pb-1 text-white'
+            className='border-solid border-2 border-white col-span-1 w-32 ml-8 md:ml-2 mt-1 md:mt-0 h-7 bg-transparent pl-1 pb-1 text-white'
             onChange={handledue}
           ></input>
         </label>
-        <select name='priority' onChange={()=>{}} onBlur={()=>{}} className='border-solid border-2 border-white h-7 text-black place-self-start md:place-self-center'>
+        <select name='priority' onChange={()=>{}} onBlur={()=>{}} className='border-solid border-2 border-white mt-1 md:mt-0 h-7 text-black md:place-self-center'>
           <option value='initial'>
             Task Priority
           </option>
@@ -106,14 +106,14 @@ const NewTaskForm = ({ createTask}: formProps) => {
         <label htmlFor='description' className='flex flex-wrap gap-1 col-span-3 md:col-span-2'>
           Description:
           <textarea
-            className='border-solid border-2 border-white w-3/4 bg-transparent pl-1 pb-1 text-white'
+            className='border-solid border-2 border-white w-full md:w-11/12 bg-transparent mb-1 md:mb-0 pl-1 pb-1 text-white md:place-items-start'
             value={newTask.description}
             placeholder='description'
             onChange={handleDescription}
           ></textarea>
         </label>
         <button
-          className='bg-green-400 text-white w-24 border-solid border-2 border-white h-8 rounded place-self-end hover:bg-green-600'
+          className='bg-green-400 text-white w-24 border-solid border-2 border-white h-8 rounded col-span-3 place-self-center md:place-self-end hover:bg-green-600'
           type='submit'
         >
           Submit
