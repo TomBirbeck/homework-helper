cf91c2e2d1ecafa7fe94be30bfc666693c960193
unknown <tombirbeck47@gmail.com>
2023-02-07 23:51:24 +0000

    added aria text to parent side

--- src/components/LogoutButton/index.tsx
+++ src/components/LogoutButton/index.tsx
@@ -4,7 +4,10 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button className="border solid-2 border-white rounded-md w-20 self-center bg-red-400 text-white hover:bg-red-600" onClick={() => logout({ returnTo: window.location.origin })}>
+    <button
+    aria-label="log out button"
+    className="border solid-2 border-white rounded-md w-20 self-center bg-red-400 text-white hover:bg-red-600"
+    onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
     </button>
   );
--- src/components/ParentTaskList/index.tsx
+++ src/components/ParentTaskList/index.tsx
@@ -23,22 +23,22 @@ const ParentTaskList: FunctionComponent<Iprops> = (props) => {
   }
 
   return (
-    <div className='flex justify-between bg-none backdrop-blur-sm border-solid border-2 border-opacity-10 border-white rounded-lg text-white text-sm md:text-base'>
+    <div ari-label='task area' className='flex justify-between bg-none backdrop-blur-sm border-solid border-2 border-opacity-10 border-white rounded-lg text-white text-sm md:text-base'>
       <div className='grid grid-cols-4 justify-between w-full'>
-        <p className='border-solid border-r-2 border-white pl-2'>
+        <p aria-label='task subject' className='border-solid border-r-2 border-white pl-2'>
           {subject}
         </p>
-        <p className='border-solid border-r-2 border-white pl-2'>
+        <p aria-label='task topic' className='border-solid border-r-2 border-white pl-2'>
           {topic}
         </p>
-        <p className='border-solid border-r-2 border-white pl-2'>{reverseDate(due)}</p>
+        <p aria-label='task due date' className='border-solid border-r-2 border-white pl-2'>{reverseDate(due)}</p>
         {completed ? (
           <div className='grid grid-cols-1 md:grid-cols-2'>
-            <p className='ml-2 text:xs text-center bg-green-400'>Completed</p>
-            <button onClick={()=>{deleteTask(id)}} className='hover:bg-green-500'>Clear</button>
+            <p aria-label='task completed button' className='ml-2 text:xs text-center bg-green-400'>Completed</p>
+            <button aria-label='clear task button' onClick={()=>{deleteTask(id)}} className='hover:bg-green-500'>Clear</button>
           </div>
         ) : (
-          <p className='ml-2 text-center bg-red-400'>Outstanding</p>
+          <p aria-label='task outstanding button' className='ml-2 text-center bg-red-400'>Outstanding</p>
         )}
       </div>
     </div>
