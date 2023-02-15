b7101f78f56af520659c4260443a9cb06f433dd6
TomBirbeck <tombirbeck47@gmail.com>
2023-02-15 13:16:13 +0000

    changed the close icon for the hamburger menu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,167 +1,283 @@
-import { useState, useEffect, ChangeEvent } from "react";
-import {MdKeyboardArrowDown, MdEdit, MdSave, MdOutlineCancel, MdOutlineContentCopy} from 'react-icons/md'
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import LogoutButton from "../LogoutButton";
-import ThemeForm from "../ThemeForm";
-import { artists } from "../../data/data";
-import ThemeContext from "../../context/ThemeContext";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect, ChangeEvent } from 'react';
+import {
+  MdKeyboardArrowDown,
+  MdEdit,
+  MdSave,
+  MdOutlineCancel,
+  MdOutlineContentCopy,
+} from 'react-icons/md';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import LogoutButton from '../LogoutButton';
+import ThemeForm from '../ThemeForm';
+import { artists } from '../../data/data';
+import ThemeContext from '../../context/ThemeContext';
+import { useAuth0 } from '@auth0/auth0-react';
 
 interface Props {
-    firstname: string;
-    surname: string;
-    studentId?: Number;
-    parentId? : Number;
-    studentCode: string;
-    email?: string;
+  firstname: string;
+  surname: string;
+  studentId?: Number;
+  parentId?: Number;
+  studentCode: string;
+  email?: string;
 }
 
-const SideMenu = (person : Props) => {
-const [openId, setOpenId] = useState(false)
-const [openAccount, setOpenAccount] = useState(false)
-const [openFirst, setOpenFirst] = useState(false)
-const [openSur, setOpenSur] = useState(false)
-const [newFirstname, setNewFirstname] = useState ({firstname: ''})
-const [newSurname, setNewSurname] = useState ({surname: ''})
-const [picture, setPicture] = useState({photo: '', artist: '', link: ''})
-const [display, setDisplay] = useState<String | null>()
-const theme = useState(ThemeContext)
-const {user} = useAuth0()
-const [accountUser, setAccountUser] = useState('')
+const SideMenu = (person: Props) => {
+  const [openId, setOpenId] = useState(false);
+  const [openAccount, setOpenAccount] = useState(false);
+  const [openFirst, setOpenFirst] = useState(false);
+  const [openSur, setOpenSur] = useState(false);
+  const [newFirstname, setNewFirstname] = useState({ firstname: '' });
+  const [newSurname, setNewSurname] = useState({ surname: '' });
+  const [picture, setPicture] = useState({ photo: '', artist: '', link: '' });
+  const [display, setDisplay] = useState<String | null>();
+  const theme = useState(ThemeContext);
+  const { user } = useAuth0();
+  const [accountUser, setAccountUser] = useState('');
 
-    async function findStudent() {
-      const checkStudent = await fetch(
-        `https://homeworkhelper.onrender.com/student?email=${user?.email}`
-      );
-      const studentData = await checkStudent.json();
-      if (studentData.payload.length > 0) {
-       setAccountUser('student'); }
-      }
-      
-      async function findParent() {
-        const parentCheck = await fetch(
-        `https://homeworkhelper.onrender.com/parent?email=${user?.email}`
-        );
-        const parentData = await parentCheck.json();
-        if (parentData.payload.length > 0) {
-        setAccountUser('parent');
-      }
+  async function findStudent() {
+    const checkStudent = await fetch(
+      `https://homeworkhelper.onrender.com/student?email=${user?.email}`
+    );
+    const studentData = await checkStudent.json();
+    if (studentData.payload.length > 0) {
+      setAccountUser('student');
     }
-  
-      useEffect(()=>{
-        findStudent()
-        findParent()
-      },[user])
-
-
-
-const findPictureDetails = (arr: {photo: string, artist: string, link: string}[]) => {
-for (let i = 0; i < arr.length; i++ ){
-    if (arr[i].photo === display){
-        setPicture(arr[i])
-    }
-}   }
-
-useEffect(()=>{
-    setDisplay(localStorage.getItem('Theme'))
-    findPictureDetails(artists)
-  },[theme])
-
-  const handleFirstnameEdit = (e:ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    setNewFirstname({...newFirstname, firstname: e.target.value})
-  }
-  const handleSurnameEdit = (e:ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    setNewSurname({...newSurname, surname: e.target.value})
   }
 
-  const patchUser = async (object:{}) => {
+  async function findParent() {
+    const parentCheck = await fetch(
+      `https://homeworkhelper.onrender.com/parent?email=${user?.email}`
+    );
+    const parentData = await parentCheck.json();
+    if (parentData.payload.length > 0) {
+      setAccountUser('parent');
+    }
+  }
+
+  useEffect(() => {
+    findStudent();
+    findParent();
+  }, [user]);
+
+  const findPictureDetails = (
+    arr: { photo: string; artist: string; link: string }[]
+  ) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].photo === display) {
+        setPicture(arr[i]);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setDisplay(localStorage.getItem('Theme'));
+    findPictureDetails(artists);
+  }, [theme]);
+
+  const handleFirstnameEdit = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setNewFirstname({ ...newFirstname, firstname: e.target.value });
+  };
+  const handleSurnameEdit = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setNewSurname({ ...newSurname, surname: e.target.value });
+  };
+
+  const patchUser = async (object: {}) => {
     // console.log("patch", object, accountUser)
-    if (accountUser === 'student'){
-        let res = await fetch(
-            `https://homeworkhelper.onrender.com/student/${person.studentId}`,
-            // `http://localhost:3001/student`,
-            {
-              method: 'PATCH',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify(object)
-            }
-          );
-          let result = await res.json();
-          // console.log("result",result)
-          return result
+    if (accountUser === 'student') {
+      let res = await fetch(
+        `https://homeworkhelper.onrender.com/student/${person.studentId}`,
+        // `http://localhost:3001/student`,
+        {
+          method: 'PATCH',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(object),
+        }
+      );
+      let result = await res.json();
+      // console.log("result",result)
+      return result;
+    } else if (accountUser === 'parent') {
+      let res = await fetch(
+        `https://homeworkhelper.onrender.com/parent/${person.parentId}`,
+        // `http://localhost:3001/parent`,
+        {
+          method: 'PATCH',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(object),
+        }
+      );
+      let result = await res.json();
+      // console.log("result",result)
+      return result;
     }
-   else if (accountUser === 'parent'){
-        let res = await fetch(
-            `https://homeworkhelper.onrender.com/parent/${person.parentId}`,
-            // `http://localhost:3001/parent`,
-            {
-              method: 'PATCH',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify(object)
-            }
-          );
-          let result = await res.json();
-          // console.log("result",result)
-          return result
-    }
-}
+  };
 
-const handleFirstnameSubmit = () => {
-    patchUser(newFirstname)
-    setOpenFirst(false)
-    setNewFirstname({...newFirstname, firstname: ''})
-}
+  const handleFirstnameSubmit = () => {
+    patchUser(newFirstname);
+    setOpenFirst(false);
+    setNewFirstname({ ...newFirstname, firstname: '' });
+  };
 
-const handleSurnameSubmit = () => {
-    patchUser(newSurname)
-    setOpenSur(false)
-    setNewSurname({...newSurname, surname: ''})
-}
+  const handleSurnameSubmit = () => {
+    patchUser(newSurname);
+    setOpenSur(false);
+    setNewSurname({ ...newSurname, surname: '' });
+  };
 
-    return (
-        <aside aria-label='aside sidebar menu' className='flex flex-col h-screen fixed right-0 top-10 z-10 w-10/12 md:w-1/4 p-2 mb-1.5 bg-none backdrop-blur-md border-solid border-2 border-opacity-10 border-white rounded-lg text-white'>
-            <div className="grid place-items-end">
-            <LogoutButton/>
+  return (
+    <aside
+      aria-label='aside sidebar menu'
+      className='flex flex-col h-screen fixed right-0 top-11 z-10 w-10/12 md:w-1/4 p-2 mb-1.5 bg-none backdrop-blur-md border-solid border-2 border-opacity-10 border-white rounded-lg text-white'
+    >
+      <div className='grid place-items-end'>
+        <LogoutButton />
+      </div>
+      <span aria-label='aside header' className='my-4'>
+        Hey {person.firstname}, Welcome to your account menu
+      </span>
+      <div className='flex flex-col h-12 mb-2'>
+        {person.studentCode && (
+          <span
+            aria-label='open student code span'
+            onClick={() => {
+              setOpenId(!openId);
+            }}
+            className='display flex flex-row items-center gap-1'
+          >
+            Show Student Code
+            <MdKeyboardArrowDown />
+          </span>
+        )}
+        {openId && (
+          <div
+            aria-label='student code'
+            className='grid grid-cols-6 place-items-center'
+          >
+            <span className='text-xs md:text-sm col-span-5 bg-black text-white'>
+              {person.studentCode}
+            </span>
+            <CopyToClipboard text={person.studentCode}>
+              <MdOutlineContentCopy className='md:text-lg hover:text-xl' />
+            </CopyToClipboard>
+          </div>
+        )}
+      </div>
+      <div>
+        <div
+          aria-label='edit account text'
+          className='grid grid-cols-3 place-items-center'
+        >
+          <span>Edit Account</span>{' '}
+          <MdEdit
+            aria-label='edit account icon'
+            className='text-xl hover:text-green-600 hover:text-2xl'
+            onClick={() => {
+              setOpenAccount(!openAccount);
+            }}
+          />
+        </div>
+        {openAccount && (
+          <div className='grid grid-col-1 mt-2 pl-1 md:pl-2'>
+            <div className='grid grid-col-1'>
+              <span aria-label='firstname label'>Firstname:</span>
+              {openFirst ? (
+                <div className='grid grid-cols-2'>
+                  <input
+                    aria-label='firstname input'
+                    type='text'
+                    placeholder={person.firstname}
+                    onChange={handleFirstnameEdit}
+                    className='text-black'
+                  ></input>
+                  <span className='grid grid-cols-2 w-1/3'>
+                    <MdSave
+                      aria-label='save firstname icon'
+                      className='text-2xl hover:text-green-600'
+                      onClick={handleFirstnameSubmit}
+                    />
+                    <MdOutlineCancel
+                      aria-label='cancel edit firstname icon'
+                      className='text-2xl hover:text-red-600'
+                      onClick={() => {
+                        setOpenFirst(false);
+                      }}
+                    />
+                  </span>
+                </div>
+              ) : (
+                <span aria-label='first name text' className='grid grid-cols-2'>
+                  {person.firstname}
+                  <MdEdit
+                    aria-label='edit firstname icon'
+                    className='text-xl hover:text-green-600 hover:text-2xl'
+                    onClick={() => setOpenFirst(!openFirst)}
+                  />
+                </span>
+              )}
             </div>
-            <span aria-label='aside header' className="my-4">Hey {person.firstname}, Welcome to your account menu</span>
-            <div className="flex flex-col h-12 mb-2">
-            {person.studentCode && <span aria-label='open student code span' onClick={()=>{setOpenId(!openId)}} className='display flex flex-row items-center gap-1'>Show Student Code<MdKeyboardArrowDown/></span>}
-            {openId && <div aria-label='student code' className="grid grid-cols-6 place-items-center"><span className="text-xs md:text-sm col-span-5 bg-black text-white">{person.studentCode}</span><CopyToClipboard text={person.studentCode}><MdOutlineContentCopy className="md:text-lg hover:text-xl"/></CopyToClipboard></div>}
+            <div className='grid grid-col-1'>
+              <span aria-label='surname label'>Surname:</span>
+              {openSur ? (
+                <div className='grid grid-cols-2'>
+                  <input
+                    aria-label='edit surname input'
+                    type='text'
+                    placeholder={person.surname}
+                    onChange={handleSurnameEdit}
+                    className='text-black'
+                  ></input>
+                  <span className='grid grid-cols-2 w-1/3'>
+                    <MdSave
+                      aria-label='save edit surname icon'
+                      className='text-2xl hover:text-green-600'
+                      onClick={handleSurnameSubmit}
+                    />
+                    <MdOutlineCancel
+                      aria-label='cancel edit surname icon'
+                      className='text-2xl hover:text-red-600'
+                      onClick={() => {
+                        setOpenSur(false);
+                      }}
+                    />
+                  </span>
+                </div>
+              ) : (
+                <span aria-label='surname text' className='grid grid-cols-2'>
+                  {person.surname}
+                  <MdEdit
+                    aria-label='edit surname icon'
+                    className='text-xl hover:text-green-600 hover:text-2xl'
+                    onClick={() => setOpenSur(!openSur)}
+                  />
+                </span>
+              )}
             </div>
-            <div>
-                <div aria-label='edit account text' className="grid grid-cols-3 place-items-center"><span>Edit Account</span> <MdEdit aria-label='edit account icon' className="text-xl hover:text-green-600 hover:text-2xl" onClick={()=>{setOpenAccount(!openAccount)}}/></div>
-                {openAccount && 
-                <div className="grid grid-col-1 mt-2 pl-1 md:pl-2">
-                    <div className="grid grid-col-1">
-                    <span aria-label='firstname label'>Firstname:</span>
-                    {openFirst ? 
-                    <div className="grid grid-cols-2">
-                        <input aria-label='firstname input' type='text' placeholder={person.firstname} onChange={handleFirstnameEdit} className='text-black'></input>
-                        <span className="grid grid-cols-2 w-1/3"><MdSave aria-label='save firstname icon' className="text-2xl hover:text-green-600" onClick={handleFirstnameSubmit}/><MdOutlineCancel aria-label='cancel edit firstname icon' className="text-2xl hover:text-red-600" onClick={()=>{setOpenFirst(false)}}/></span>
-                    </div> : 
-                    <span aria-label='first name text' className="grid grid-cols-2">{person.firstname}<MdEdit aria-label='edit firstname icon' className="text-xl hover:text-green-600 hover:text-2xl" onClick={()=> setOpenFirst(!openFirst)}/></span>}
-                    </div>
-                    <div className="grid grid-col-1">
-                    <span aria-label='surname label'>Surname:</span>
-                    {openSur ? 
-                    <div className="grid grid-cols-2">
-                        <input aria-label='edit surname input' type='text' placeholder={person.surname} onChange={handleSurnameEdit} className='text-black'></input>
-                        <span className="grid grid-cols-2 w-1/3"><MdSave aria-label='save edit surname icon' className="text-2xl hover:text-green-600" onClick={handleSurnameSubmit}/><MdOutlineCancel aria-label='cancel edit surname icon' className="text-2xl hover:text-red-600" onClick={()=>{setOpenSur(false)}}/></span>
-                    </div> : 
-                    <span aria-label='surname text' className="grid grid-cols-2">{person.surname}<MdEdit aria-label='edit surname icon' className="text-xl hover:text-green-600 hover:text-2xl" onClick={()=> setOpenSur(!openSur)}/></span>}
-                    </div>
-                    <span aria-label='email label'>Email:</span>
-                    <span aria-label='email text'>{person.email}</span>
-                </div>}
-            </div>
-            <ThemeForm/>
-            <div className="h-1/2 flex items-end">
-            <span aria-label='background image information area' className="grid grid-cols-2 gap-1 text-sm md:text-base"><p>Background Image by</p><a href={picture.link} target='_blank' className="hover:text-blue-900 text-sm md:text-base">{picture.artist}</a></span>
-            </div>
-        </aside>
-    )
-}
+            <span aria-label='email label'>Email:</span>
+            <span aria-label='email text'>{person.email}</span>
+          </div>
+        )}
+      </div>
+      <ThemeForm />
+      <div className='h-1/2 flex items-end'>
+        <span
+          aria-label='background image information area'
+          className='grid grid-cols-2 gap-1 text-sm md:text-base'
+        >
+          <p>Background Image by</p>
+          <a
+            href={picture.link}
+            target='_blank'
+            className='hover:text-blue-900 text-sm md:text-base'
+          >
+            {picture.artist}
+          </a>
+        </span>
+      </div>
+    </aside>
+  );
+};
 
-export default SideMenu
+export default SideMenu;
--- src/components/StudentHomepage/index.tsx
+++ src/components/StudentHomepage/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
-import {GiHamburgerMenu} from 'react-icons/gi'
+import { GiHamburgerMenu, GiCancel } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 import Tasks from '../Tasks';
 import list from '../../data/data';
 import NewTaskForm from '../NewTaskForm';
@@ -18,8 +19,7 @@ type API = {
   priority: string;
   completed: boolean;
   deleted: boolean;
-}
-
+};
 
 const StudentHomepage = () => {
   const [tasks, setTasks] = useState<Array<any>>(list);
@@ -29,103 +29,173 @@ const StudentHomepage = () => {
     surname: string;
     studentEmail: string;
     student_code: string;
-  }>({ student_id: 0, firstname: '', surname: '', studentEmail: '', student_code: '' });
+  }>({
+    student_id: 0,
+    firstname: '',
+    surname: '',
+    studentEmail: '',
+    student_code: '',
+  });
   const [progress, setProgress] = useState(0);
   const [total, setTotal] = useState(0);
-  const [openMenu, setOpenMenu] = useState(false)
-  const [openStaxx, setOpenStaxx] = useState(false)
-  const {user} = useAuth0()
-  const display = useContext(ThemeContext)
-  const [theme, setTheme] = useState<String | null>()
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openStaxx, setOpenStaxx] = useState(false);
+  const { user } = useAuth0();
+  const display = useContext(ThemeContext);
+  const [theme, setTheme] = useState<String | null>();
 
-  
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem('Theme', display[0]);
-    setTheme(localStorage.getItem('Theme'))
-  },[display])
-  
+    setTheme(localStorage.getItem('Theme'));
+  }, [display]);
+
   // console.log(user)
-  
+
   async function getStudent() {
     const res = await fetch(
       `https://homeworkhelper.onrender.com/student?email=${user?.email}`
     );
     const data = await res.json();
     // console.log("student", data.payload[0])
-      setStudent({...student, student_id: data.payload[0].student_id, firstname: data.payload[0].firstname, surname: data.payload[0].surname, studentEmail: data.payload[0].email, student_code: data.payload[0].student_code});
+    setStudent({
+      ...student,
+      student_id: data.payload[0].student_id,
+      firstname: data.payload[0].firstname,
+      surname: data.payload[0].surname,
+      studentEmail: data.payload[0].email,
+      student_code: data.payload[0].student_code,
+    });
   }
 
   // console.log("student", student)
 
   async function getTasks() {
-      const res = await fetch(
-        // `http://localhost:3001/studenttasks?code=${student?.student_code}`
-        `https://homeworkhelper.onrender.com/studenttasks?code=${student.student_code}`
-      );
-      const data = await res.json();
-      const tasks = data.payload?.filter((task: API) => {return task.deleted === false})
-      setTasks(tasks);
-      setTotal(tasks.length);
-      let count = 0
-      for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].completed) {
-          count++
-        }
-        setProgress(count);
+    const res = await fetch(
+      // `http://localhost:3001/studenttasks?code=${student?.student_code}`
+      `https://homeworkhelper.onrender.com/studenttasks?code=${student.student_code}`
+    );
+    const data = await res.json();
+    const tasks = data.payload?.filter((task: API) => {
+      return task.deleted === false;
+    });
+    setTasks(tasks);
+    setTotal(tasks.length);
+    let count = 0;
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].completed) {
+        count++;
+      }
+      setProgress(count);
     }
     // console.log('get tasks ran', student?.student_code);
   }
 
   async function createTask(task: Tasks) {
-    const code = student.student_code
-    
-    let res = await fetch(
-      `https://homeworkhelper.onrender.com/tasks/${code}`,
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(task),
-      }
-    );
+    const code = student.student_code;
+
+    let res = await fetch(`https://homeworkhelper.onrender.com/tasks/${code}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(task),
+    });
     let result = await res.json();
     // console.log('new task posted', result);
     getTasks();
-    setOpenStaxx(false)
+    setOpenStaxx(false);
   }
 
   useEffect(() => {
     getStudent();
-    getTasks()
-  },[student.student_code]);
+    getTasks();
+  }, [student.student_code]);
 
   return (
-    <div aria-label='student homepage' className={theme === 'tree'? 'm-0 p-2 bg-cover bg-tree min-h-screen w-100vw'
-    : theme === 'universe'? 'm-0 p-2 bg-cover bg-universe min-h-screen w-100vw' 
-    : theme === 'stream'? 'm-0 p-2 bg-cover bg-stream min-h-screen w-100vw' 
-    : theme === 'ruin'? 'm-0 p-2 bg-cover bg-ruin min-h-screen w-100vw' 
-    : theme === 'aurora' ? 'm-0 p-2 bg-cover bg-aurora min-h-screen w-100vw' 
-    : 'm-0 p-2 bg-teal-800 min-h-screen w-100vw'} >
-      <span aria-label='hamburger menu' className='z-10 fixed right-0 top-0 text-white text-xl md:text-4xl mt-2 grid grid-cols-2 place-items-center' onClick={()=>{setOpenMenu(!openMenu)}}><GiHamburgerMenu/></span>
-      {openMenu && <SideMenu firstname={student.firstname} surname={student.surname} studentId={student.student_id} email={student.studentEmail} studentCode={student.student_code}/>}
-        {student && (
-          <div>
-          <h1 aria-label='study staxx title' className={theme === 'boat' ? 'font-bold text-black text-base md:text-4xl mb-2 text-center' :'italic font-bold text-white text-base md:text-4xl mb-2 text-center'}>
+    <div
+      aria-label='student homepage'
+      className={
+        theme === 'tree'
+          ? 'm-0 p-2 bg-cover bg-tree min-h-screen w-100vw'
+          : theme === 'universe'
+          ? 'm-0 p-2 bg-cover bg-universe min-h-screen w-100vw'
+          : theme === 'stream'
+          ? 'm-0 p-2 bg-cover bg-stream min-h-screen w-100vw'
+          : theme === 'ruin'
+          ? 'm-0 p-2 bg-cover bg-ruin min-h-screen w-100vw'
+          : theme === 'aurora'
+          ? 'm-0 p-2 bg-cover bg-aurora min-h-screen w-100vw'
+          : 'm-0 p-2 bg-teal-800 min-h-screen w-100vw'
+      }
+    >
+      {openMenu ? (
+        <span
+          aria-label='hamburger menu'
+          className='z-10 fixed right-0 top-0 text-white text-xl md:text-4xl mt-2 grid grid-cols-2 place-items-center'
+          onClick={() => {
+            setOpenMenu(false);
+          }}
+        >
+          <GiCancel />
+        </span>
+      ) : (
+        <span
+          aria-label='hamburger menu'
+          className='z-10 fixed right-0 top-0 text-white text-xl md:text-4xl mt-2 grid grid-cols-2 place-items-center'
+          onClick={() => {
+            setOpenMenu(true);
+          }}
+        >
+          <GiHamburgerMenu />
+        </span>
+      )}
+      {openMenu && (
+        <SideMenu
+          firstname={student.firstname}
+          surname={student.surname}
+          studentId={student.student_id}
+          email={student.studentEmail}
+          studentCode={student.student_code}
+        />
+      )}
+      {student && (
+        <div>
+          <h1
+            aria-label='study staxx title'
+            className={
+              theme === 'boat'
+                ? 'font-bold text-black text-base md:text-4xl mb-2 text-center'
+                : 'italic font-bold text-white text-base md:text-4xl mb-2 text-center'
+            }
+          >
             Welcome to Study Staxx
           </h1>
-          <h2 aria-label='subtitle' className={theme === 'boat' ? 'font-bold text-black text-xs md:text-xl mb-4 text-center' :'italic font-bold text-white text-xs md:text-xl mb-4 text-center'}>
-            Track, Stack & Succeed</h2>
-          </div>
-        )}
-        <GetTasksContext.Provider value={getTasks}>
-          <div className='grid place-items-center my-3'>
-        <h3 aria-label='add staxx label' className='italic font-bold md:text-2xl text-center text-white font-mono w-fit' onClick={()=>{setOpenStaxx(!openStaxx)}}>
-        Add Staxx {!openStaxx ? <span>+</span> : <span>-</span>}
-      </h3>
-      </div>
-        {openStaxx && <NewTaskForm createTask={createTask}/>}
-         <Tasks tasks={tasks} getTasks={getTasks}/>
-        </GetTasksContext.Provider>
-        <ProgressBar progress={progress} total={total}/>
+          <h2
+            aria-label='subtitle'
+            className={
+              theme === 'boat'
+                ? 'font-bold text-black text-xs md:text-xl mb-4 text-center'
+                : 'italic font-bold text-white text-xs md:text-xl mb-4 text-center'
+            }
+          >
+            Track, Stack & Succeed
+          </h2>
+        </div>
+      )}
+      <GetTasksContext.Provider value={getTasks}>
+        <div className='grid place-items-center my-3'>
+          <h3
+            aria-label='add staxx label'
+            className='italic font-bold md:text-2xl text-center text-white font-mono w-fit'
+            onClick={() => {
+              setOpenStaxx(!openStaxx);
+            }}
+          >
+            Add Staxx {!openStaxx ? <span>+</span> : <span>-</span>}
+          </h3>
+        </div>
+        {openStaxx && <NewTaskForm createTask={createTask} />}
+        <Tasks tasks={tasks} getTasks={getTasks} />
+      </GetTasksContext.Provider>
+      <ProgressBar progress={progress} total={total} />
     </div>
   );
 };
